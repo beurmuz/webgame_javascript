3c405d318339400ca02f78aeb8c0b83e09f4bfbe
beurmuz <fallinta2@gmail.com>
2021-08-15 19:32:28 +0900

    새로고침 기능, 칸수 조절, 상대방 기능 추가

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,14 +1,29 @@
-// [
-//     [td00, td01, td02],
-//     [td10, td11, td12],
-//     [td20, td21, td22],
-// ]
-
 const { body } = document; // const body = document.body;와 같음
 const $table = document.createElement('table');
 const $result = document.createElement('div'); // 결과창
+const $btn = document.querySelector('#btn'); // 재시작 버튼 
 const rows = [];
 let turn = 'O';
+
+
+const checkWinnerAndDraw = (target) => {
+    const hasWinner = checkWinner(target);
+
+    // 승자가 있으면 
+    if(hasWinner) {
+        $result.textContent = `${turn}님이 승리!`;
+        $table.removeEventListener('click', callback);
+        return;
+    }
+    
+    // 승자가 없으면
+    const draw = rows.flat().every((cell) => cell.textContent);
+    if(draw) {
+        $result.textContent = '무승부';
+        return;
+    };
+    turn = turn === 'X' ? 'O' : 'X';
+};
 
 // 승자인지 판단하는 함수 
 const checkWinner = (target) => {
@@ -23,7 +38,9 @@ const checkWinner = (target) => {
     // 가로줄 검사
     if( rows[rowIndex][0].textContent === turn &&
         rows[rowIndex][1].textContent === turn && 
-        rows[rowIndex][2].textContent === turn
+        rows[rowIndex][2].textContent === turn && 
+        rows[rowIndex][3].textContent === turn && 
+        rows[rowIndex][4].textContent === turn 
     ) {
         hasWinner = true;
     }
@@ -31,27 +48,38 @@ const checkWinner = (target) => {
     // 세로줄 검사
     if( rows[0][cellIndex].textContent === turn &&
         rows[1][cellIndex].textContent === turn &&
-        rows[2][cellIndex].textContent === turn
+        rows[2][cellIndex].textContent === turn &&
+        rows[3][cellIndex].textContent === turn &&
+        rows[4][cellIndex].textContent === turn
     ) {
         hasWinner = true;
     }
     // 대각선 검사 
     if( rows[0][0].textContent === turn &&
         rows[1][1].textContent === turn &&
-        rows[2][2].textContent === turn
+        rows[2][2].textContent === turn &&
+        rows[3][3].textContent === turn &&
+        rows[4][4].textContent === turn
     ) { 
         hasWinner = true;
     }
-    if( rows[0][2].textContent === turn &&
-        rows[1][1].textContent === turn &&
-        rows[2][0].textContent === turn
+    if( rows[0][4].textContent === turn &&
+        rows[1][3].textContent === turn &&
+        rows[2][2].textContent === turn &&
+        rows[3][1].textContent === turn &&
+        rows[4][0].textContent === turn 
     ) { 
         hasWinner = true;
     }
     return hasWinner;
 };
 
+let clickable = true;
 const callback = (event) => {
+    if(!clickable) {
+        return;
+    }
+
     if(event.target.textContent !== '') { // 빈 칸이 아니면
         console.log('빈 칸이 아닙니다.');
         return;
@@ -60,27 +88,26 @@ const callback = (event) => {
     //빈칸이면 
     console.log('빈 칸 입니다.');
     event.target.textContent = turn;
-    const hasWinner = checkWinner(event.target);
+    checkWinnerAndDraw(event.target);
     
-    // 승자가 있으면
-    if(hasWinner) {
-        $result.textContent = `${turn}님이 승리!`;
-        $table.removeEventListener('click', callback);
-        return;
+    
+    // 컴퓨터 동작 추가
+    if(turn === 'X') {
+        const emptyCells = rows.flat().filter((v) => !v.textContent);
+        const randomCell = emptyCells[Math.floor(Math.random()*emptyCells.length)];
+        clickable = false;
+        setTimeout(() => {
+            randomCell.textContent = 'X';
+            checkWinnerAndDraw(randomCell);
+            clickable = true;
+        }, 500);
     }
-    // 승자가 없으면
-    const draw = rows.flat().every((cell) => cell.textContent);
-    if(draw) {
-        $result.textContent = `무승부`;
-        return;
-    }
-    turn = turn === 'X' ? 'O' : 'X';
 };
 
-for(let i = 1; i <= 3; i++) {
+for(let i = 1; i <= 5; i++) {
     const $tr = document.createElement('tr');
     const cells = [];
-    for(let j = 1; j <= 3; j++) {
+    for(let j = 1; j <= 5; j++) {
         const $td = document.createElement('td');
         //$td.addEventListener('click', callback); // 각 칸에 모두 이벤트 리스너를 추가함
         cells.push($td);
@@ -92,3 +119,7 @@ for(let i = 1; i <= 3; i++) {
 }
 body.appendChild($table);
 body.appendChild($result);
+
+$btn.addEventListener('click', () => {
+    location.reload();
+});
